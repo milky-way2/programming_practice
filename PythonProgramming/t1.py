c0825fa8eaad562cb4e4dba5b1c04790cf62d97b
milky-way2 <soham61chakrabortty@gmail.com>
2021-09-29 14:55:42 +0530

    hello

--- PythonProgramming/t1.py
+++ PythonProgramming/t1.py
@@ -1,3 +1,21 @@
 print("hello")
 print("hello world")
 print("hello","world")
+print('hello')
+print('''hello''')
+print('Ram says "hello"')
+print("Ram says 'hello'")
+print('''Ram says "hello" and i says 'hi' ''')
+a=input("enter first number")
+b=input("enter second number")
+s=a+b
+print("Sum=",s)
+a=int (input("enter first number"))
+b=int (input("enter second number"))
+s=a+b
+print("Sum=",s)
+if a>b:
+    print("a is grater");
+else :
+        print("b is greter");
+
