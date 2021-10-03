b087b5f7cae5246048c56ddb8ecce2e0ec107a03
milky-way2 <soham61chakrabortty@gmail.com>
2021-10-03 14:23:35 +0530

    Added new .py file

---
+++ PythonProgramming/simple_interest.py
@@ -0,0 +1,7 @@
+pa=float (input("Enter principle amount : "))
+t=int (input("Enter number of year : "))
+r=float(input("Enter rate of interest : "))
+i=(pa*r*t)/100
+ta=pa+i
+print("Amount of interest : ",i)
+print("Total principle amount after interest calculation : ",ta)
