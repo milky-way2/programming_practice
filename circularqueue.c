a0ca3e7c7d29c0a4771d360b8152445687975cb3
milky-way2 <soham61chakrabortty@gmail.com>
2021-11-12 09:50:49 +0530

    added new file

---
+++ circularqueue.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#define MAX 5
+void insert();
+void delete();
+void display();
+int queue_array[MAX];
+int front= -1;
+int rear= -1;
+
+int item;
+void main()
+{
+int choice;
+while (1)
+{
+    printf("\n\n  1.insert element to stack \n");
+    printf("  2.delete element from stack \n");
+    printf("  3.Display all elements of stack \n");
+    printf("  4.Quit\n");
+     printf("Enter your choice : ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1:
+        insert();
+        break;
+        case 2:
+        delete();
+        break;
+        case 3:
+        display();
+        break;
+        case 4:
+        exit(1);
+        default:
+        printf("Wrong choce \n Try again \n");
+
+
+    }
+
+}
+}
+void insert()
+{
+    int item;
+   
+    if(rear==MAX-1)
+    {
+        if(front !=0)
+        {
+             rear=0;
+             front=0;
+     printf("\nEnter the element : ");
+    scanf("%d",&item);
+            
+        }
+        else
+        {
+            printf("\n The Queue is full \n");
+        return; 
+        }
+       
+
+    }
+       
+ 
+    
+   
+    else
+    {
+         printf("\nEnter the element : ");
+    scanf("%d",&item);
+     if(front==-1 && rear ==-1)
+    {
+        front=0;
+        rear=0;
+    }
+    else
+    {
+        rear++;
+    }
+        
+    }
+    queue_array[rear]=item;
+printf("\nThe value'%d' has been inserted",item);   
+}
+void delete()
+{
+    
+    int item;
+
+    // if(front==-1 || front>rear)    
+    if(front==-1 && rear==-1)    
+
+    {
+        printf("\nThe queue is empty\n");
+       return;
+    }
+    else
+    {
+item=queue_array[front];
+if(front==rear)
+{
+    front=-1;
+    rear=-1;
+}
+else 
+{
+    front++;
+}
+printf("\n The deleted element is %d",item);
+    }
+}
+void display()
+{
+    int i;
+    if(rear==-1)
+    {
+        printf("\nThe queue is empty\n");
+    }
+    else
+    {
+      printf("\n The queue is : \n");
+      if(rear=0)
+      {
+           for(i=front;i<=rear;i++,rear=MAX-1)
+      {
+          printf("%d\t",queue_array[i]);
+      }
+      }
+      else
+      {
+         for(i=front;i<=rear;i++)
+      {
+          printf("%d\t",queue_array[i]);
+      }  
+      }
+     
+    }
+}
---
+++ stack.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#define MAX 5
+void push();
+void pop();
+void display();
+int stack_array[MAX];
+int top =-1;
+int item;
+void  main()
+ {
+int choice;
+while (1)
+{
+    printf("\n\n   1. Push element to stack \n");
+    printf("   2. Pop element from stack \n");
+    printf("   3. Display all elements of stack \n");
+    printf("   4. Quit\n");
+    printf("Enter your choice : ");
+    scanf("%d",&choice);
+    switch (choice)
+    {
+        case 1:
+        push();
+        break;
+        case 2:
+        pop();
+        break;
+        case 3:
+        display();
+        break;
+        case 4:
+        exit(1);
+        default:
+        printf("Wrong choce  \n Try again : \n");
+
+
+    }
+
+}
+}
+void push()
+{
+    int item;
+    if(top==MAX-1)
+    {
+        printf("\n The stack is full \n");
+
+    }
+    else
+    {
+printf("\nEnter the element : ");
+scanf("%d",&item);
+top++;
+stack_array[top]=item;
+  printf("\nThe value'%d' has been inserted",item); 
+   }
+      
+}
+void pop()
+{
+    if(top==-1)
+    {
+        printf("\nThe stack is empty\n");
+
+    }
+    else
+    {
+item=stack_array[top];
+printf("\n The deleted element is %d",stack_array[top]);
+top--;
+    }
+}
+void display()
+{
+    int i;
+    if(top==-1)
+    {
+        printf("\nThe stack is empty\n");
+    }
+    else
+    {
+      printf("\n The Stack is  : \n");
+      for(i=top;i>=0;i--)
+      {
+          printf("%d\n",stack_array[i]);
+      }
+    }
+}
